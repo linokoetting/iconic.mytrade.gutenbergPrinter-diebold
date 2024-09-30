7010145848327aeeb02dc013b78cbc49838031e0
lino <lino@lino-XPS-13-9370>
2024-09-30 16:54:42 +0200

    in progress

--- src/main/java/iconic/mytrade/gutenbergPrinter/PrinterCommands.java
+++ src/main/java/iconic/mytrade/gutenbergPrinter/PrinterCommands.java
@@ -602,6 +602,8 @@ public class PrinterCommands extends iconic.mytrade.gutenbergInterface.PrinterCo
 		{
             if ((int)(newdes.charAt(k2idx)) >= 128)
                 k2desc = k2desc + ".";
+            else if ((int)(newdes.charAt(k2idx)) < 32)
+                k2desc = k2desc + " ";
             else
                 k2desc = k2desc + newdes.charAt(k2idx);
 		}
@@ -1218,6 +1220,8 @@ public class PrinterCommands extends iconic.mytrade.gutenbergInterface.PrinterCo
 		{
 	        if ((int)(newdes.charAt(k2idx)) >= 128)
 	            k2desc = k2desc + ".";
+            else if ((int)(newdes.charAt(k2idx)) < 32)
+                k2desc = k2desc + " ";
 	        else
 	            k2desc = k2desc + newdes.charAt(k2idx);
 		}
